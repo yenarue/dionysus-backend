473a57a58438ac2b90461ce3ca5292975160963f
yenarue <yenarue@gmail.com>
2020-11-06 16:01:16 +0900

    fix: 구글스프레드시트 변수명 오류 수정

--- utils/googleSpreadSheets.js
+++ utils/googleSpreadSheets.js
@@ -2,7 +2,7 @@ const GoogleSpreadsheet = require('google-spreadsheet').GoogleSpreadsheet;
 const credentials = require('../google-docs-credentials.json');
 
 const getDocument = async (spreadsheetUrl) => {
-  const idMatchResults = spreadsheet.match(/https:\/\/docs\.google\.com\/spreadsheets\/d\/([A-Za-z0-9_-]*)[?]?.*/);
+  const idMatchResults = spreadsheetUrl.match(/https:\/\/docs\.google\.com\/spreadsheets\/d\/([A-Za-z0-9_-]*)[?]?.*/);
   const sheetId = idMatchResults[1];
 
   const doc = new GoogleSpreadsheet(sheetId);
